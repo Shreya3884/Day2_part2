b29d1a74be28d35d400849b965729fc60b3664c7
Shreya3884 <sj0711778@gmail.com>
2026-01-19 15:25:31 +0530

    first commit

--- src/main/java/com/example/day2part2/controller/Home.java
+++ src/main/java/com/example/day2part2/controller/Home.java
@@ -2,6 +2,10 @@ package com.example.day2part2.controller;
 import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Home {
     @GetMapping("/about")
@@ -9,10 +13,16 @@ public class Home {
         return "this is all about home page";
     }
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student= new StudentModel(1,"shreya","jainjdh");
-        return student;
+    public List<StudentModel> getStudent(){
+        List<StudentModel> student= new ArrayList<>();
+         student.add(new StudentModel(1,"shreya","shreyagmail.com"));
+        student.add(new StudentModel(1,"jahnvi","jahnvigmail.com"));
+        student.add(new StudentModel(1,"mani","manigmail.com"));
+        student.add(new StudentModel(1,"suhani","suhanigmail.com"));
+        student.add(new StudentModel(1,"shre","shregmail.com"));
+                return student;
     }
+
 }
 
 
